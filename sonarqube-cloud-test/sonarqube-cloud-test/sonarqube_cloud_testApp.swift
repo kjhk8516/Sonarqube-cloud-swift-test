ae0ded8691b62c338b539efad98dea39e66a7381
kjhk8516 <70681333+kjhk8516@users.noreply.github.com>
2024-12-04 13:42:04 +0900

    TestProject setup

---
+++ sonarqube-cloud-test/sonarqube-cloud-test/sonarqube_cloud_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  sonarqube_cloud_testApp.swift
+//  sonarqube-cloud-test
+//
+//  Created by ethan.jhkim on 12/4/24.
+//
+
+import SwiftUI
+
+@main
+struct sonarqube_cloud_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sonarqube-cloud-test/sonarqube-cloud-testTests/sonarqube_cloud_testTests.swift
@@ -0,0 +1,17 @@
+//
+//  sonarqube_cloud_testTests.swift
+//  sonarqube-cloud-testTests
+//
+//  Created by ethan.jhkim on 12/4/24.
+//
+
+import Testing
+@testable import sonarqube_cloud_test
+
+struct sonarqube_cloud_testTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
